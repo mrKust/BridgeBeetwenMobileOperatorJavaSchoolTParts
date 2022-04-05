af546751e97b5354253ab60ff8851e5ec9d6de9a
mrKust <dimvas2010@yandex.ru>
2022-04-06 01:11:44 +0300

    lil progress

--- src/main/java/com/school/bean/ejb/RabbitReceiverControl.java
+++ src/main/java/com/school/bean/ejb/RabbitReceiverControl.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
@@ -15,29 +17,42 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
- @Startup
- @Singleton
- public class RabbitReceiverControl {
-     private final static String QUEUE_NAME = "queue1";
+@Singleton
+@Startup
+public class RabbitReceiverControl {
 
-     @PostConstruct
-     void init() throws IOException, TimeoutException {
-         ConnectionFactory factory = new ConnectionFactory();
-         factory.setHost("localhost");
-         Connection connection = null;
-         connection = factory.newConnection();
-         Channel channel = null;
-         channel = connection.createChannel();
-         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-         System.out.println(" [*] Waiting for messages.");
-         Consumer consumer = new DefaultConsumer(channel) {
-             @Override
-             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-                                        byte[] body) throws IOException {
-                 String message = new String(body, "UTF-8");
-                 System.out.println(" [x] Received '" + message + "'");
-             }
-         };
-         channel.basicConsume(QUEUE_NAME, true, consumer);
-     }
- }
+    private final static String QUEUE_NAME = "queue1";
+    private ConnectionFactory factory = null;
+    private Connection connection = null;
+    private Channel channel = null;
+
+    @EJB
+    TariffsInfo tariffsInfo;
+
+    @PostConstruct
+    void init() throws IOException, TimeoutException {
+        factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages.");
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+                tariffsInfo.updateInfo();
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+
+    @PreDestroy
+    private void close() throws IOException, TimeoutException {
+        channel.close();
+        connection.close();
+    }
+}
+
--- src/main/java/com/school/bean/ejb/TariffsInfo.java
+++ src/main/java/com/school/bean/ejb/TariffsInfo.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
-@NoArgsConstructor
 public class TariffsInfo {
 
     @Getter
